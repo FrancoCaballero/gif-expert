6b6ef06fb87090ccabded3740532d35780c65c16
Franco Caballero <franco.caballero.v@gmail.com>
2022-02-03 14:30:33 -0300

    First commit

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
+import { getGifs } from "../helpers/getGifs";
+
+export const useFetchGifs = (category) => {
+  const [state, setState] = useState({ data: [], loading: true });
+
+  useEffect(() => {
+    getGifs(category).then((gifs) => setState({ data: gifs, loading: false }));
+  }, [category]);
+
+  return state;
+};
